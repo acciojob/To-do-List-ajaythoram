f5f0592a886b5b5f354295e2e84f8574e36e51f4
ajaythoram <115677014+ajaythoram@users.noreply.github.com>
2023-05-11 11:42:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 //your code here
-
+const txt = document.getElementById("newTodoInput");
+const btn = document.getElementById("addTodoBtn");
+  function add() {
+  	let text = txt.value.trim();
+	  const li = document.creatElement("li");
+	  li.innerText = text;
+	  const ul = document.getElementById("todoList");
+	  ul.appendChild(li);
+  }
