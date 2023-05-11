43b1a1256f5ff84fa4786acedf8ecf3b32fa02eb
ajaythoram <115677014+ajaythoram@users.noreply.github.com>
2023-05-11 11:52:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ const txt = document.getElementById("newTodoInput");
 const btn = document.getElementById("addTodoBtn");
   function add() {
   	let text = txt.value.trim();
+	  if (text.length === 0) { // don't add empty todo items
+      return;
+    }
 	  const li = document.createElement("li");
 	  li.innerText = text;
 	  const ul = document.getElementById("todoList");
 	  ul.appendChild(li);
+	  txt.value = "";
   }
